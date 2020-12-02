c65384d3f41eba09b2ce3b8a083baf29f0871db3
kjedmeade <kjedmeade@gmail.com>
2020-12-02 16:20:29 -0700

    changes to front end

--- ngBooking/src/app/components/bookings/bookings.component.ts
+++ ngBooking/src/app/components/bookings/bookings.component.ts
@@ -1,7 +1,6 @@
-import { BookingService } from './../../services/booking.service';
 import { Component, OnInit } from '@angular/core';
 import { Booking } from 'src/app/models/booking';
-
+import { BookingService } from 'src/app/services/booking.service';
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.component.html',
@@ -11,7 +10,8 @@ export class BookingsComponent implements OnInit {
   bookings: Booking[] =[];
   selected: Booking = null;
   newBooking: Booking = new Booking();
-
+  updatedBooking: Booking = null;
+  avgDuration: number =0;
 
 
   constructor(
@@ -19,6 +19,7 @@ export class BookingsComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadBookings();
+    this.averageDuration();
   }
 
   loadBookings(): void{
@@ -48,10 +49,57 @@ console.log(err);
 
         });
         this.newBooking = new Booking();
+        this.selected=null;
     }
 
   displayTodo(todo) {
     this.selected = todo;
   }
+
+  updateBooking(booking: Booking) {
+    // this.todos[todo.id - 1] = Object.assign({}, todo);
+    // this.selected = Object.assign({}, todo);
+    this.bServe.update(booking).subscribe(
+      (good) => {
+        this.loadBookings();
+        if (this.selected != null) {
+          this.selected = Object.assign({}, booking);
+        }
+      },
+      (bad) => {
+        console.error(bad);
+      }
+    );
+    this.updatedBooking = null;
+  }
+
+  deleteBooking(id) {
+    this.bServe.destroy(id).subscribe(
+      (good) => {
+        this.loadBookings();
+      },
+      (bad) => {
+        console.error(bad);
+      }
+    );
+    this.selected=null;
+  }
+
+  setUpdatedBooking() {
+    this.updatedBooking = Object.assign({}, this.selected);
+  }
+
+averageDuration=function () {
+  var count:  number =0;
+  var total: number = 0;
+  for (let i=0; i< this.bookings.length; i++){
+    total += this.bookings[i].duration;
+    count ++;
+  }
+  let average = total/count;
+  this.avgDuration = average;
+};
+
+
 
 }
--- ngBooking/src/app/services/booking.service.ts
+++ ngBooking/src/app/services/booking.service.ts
@@ -30,6 +30,29 @@ export class BookingService {
         })
       );
   }
+  update(booking: Booking) {
+    const httpOptions = {
+      headers: {
+        'Content-type': 'application/json'
+      }
+    };
+    return this.http.put<Booking[]>(this.baseUrl + '/' + booking.id, booking, httpOptions).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('BookingService: Error retrieving todo list');
+      })
+    );
+  }
+
+
+  destroy(bookingId: number): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.url}/${bookingId}`).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('BookingService.destroy(): Error deleting todo');
+      })
+    );
+  }
 
   constructor(
     private http: HttpClient
